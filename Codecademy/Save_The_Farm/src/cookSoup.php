1c4ddce14a00d989c1e6bc730c4371b1b84ce0a6
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2023-07-05 21:49:10 -0700

    cmit_07/05/2023_9:49:PM

--- Codecademy/Save_The_Farm/src/cookSoup.php
+++ Codecademy/Save_The_Farm/src/cookSoup.php
@@ -2,7 +2,16 @@
   
 function cookSoup(){
 	// Write your code here:
-  
+	global $location, $has_mushroom, $has_soup;
+
+	if(!($location === "kitchen" && $has_mushroom)){
+		echo "You can't cook like this! You need something to cook AND to be in the kitchen.\n";
+
+	}else{
+		echo "You made some mushroom soup. Mushroom is the queen of all soups!\n";
+		$has_mushroom = FALSE;
+		$has_soup = TRUE;
+	}
   
   
   
